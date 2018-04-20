80591a73596a659b6bb0d66191eeed4fa483c008
cupcakes3000 <zcabpou@ucl.ac.uk>
2018-04-20 20:43:19 +0100

    fixed player and added art improvements

--- game_demo/Assets/Scripts/changePlayer.cs
+++ game_demo/Assets/Scripts/changePlayer.cs
@@ -1,4 +1,4 @@
-﻿/* learnt from this youtube tutorial : https://www.youtube.com/watch?v=1EJOYWBcrzQ */
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +8,6 @@ public class ChangePlayer : MonoBehaviour {
 	public Sprite manLeft;
 	public Sprite manRight; //switching in between sprites
 
-	// be accessed outside of the class so we can modify when the player needs switching
-	public float timer = 3f;
-	public float delay = 3f;
 
 	// initialize the starting sprite to at least one of them
 	void Start(){
@@ -19,16 +16,13 @@ public class ChangePlayer : MonoBehaviour {
 
 	void Update(){
 
-		timer = timer - Time.deltaTime;
-		if(timer < 0){
+		if(Input.GetKeyDown("space")){
 			if (this.gameObject.GetComponent<SpriteRenderer>().sprite == manLeft) {
 				this.gameObject.GetComponent<SpriteRenderer> ().sprite = manRight;
-				timer = delay; //resetting the timer
 				return;
 			}
 			if (this.gameObject.GetComponent<SpriteRenderer> ().sprite == manRight) {
 				this.gameObject.GetComponent<SpriteRenderer> ().sprite = manLeft;
-				timer = delay; //resetting the timer
 				return;
 			}
 
