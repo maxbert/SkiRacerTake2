16cc4ac414a4bc0b645c07badd5cf5e81c3bdb39
maxbert <max@bertfield.com>
2018-04-23 08:31:04 +0100

    fogot about all those assets i created

---
+++ game_demo/Assets/Scripts/BreathsCount.cs
@@ -0,0 +1,16 @@
+public static class BreathsCount
+{
+    private static int breathsPer;
+
+    public static int BreathsPer
+    {
+        get
+        {
+            return breathsPer;
+        }
+        set
+        {
+            breathsPer = value;
+        }
+    }
+}
